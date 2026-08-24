b65cb979c861dbac6bb669aa2b513f3a13786243
Parag Ekbote <thecoolekbote189@gmail.com>
2026-08-24 10:53:23 +0000

    add config for test run as well.

--- carbon-enrichment/carbon_enrichment/schema.py
+++ carbon-enrichment/carbon_enrichment/schema.py
@@ -73,6 +73,8 @@ STREAMING_SPLIT: Final[str] = "train"
 
 VALIDATION_LEVEL_ROWS: Final[dict[str, int]] = {
     "dev": 1000,
+    "dev_gpu_small": 3_000,
+    "dev_gpu_medium": 30_000,
     "integration": 1_000_000,
     "auth": 32_410_000,
 }
